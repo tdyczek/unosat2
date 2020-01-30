7d72517f829568547d6d3221dcfb26085deeba63
t2 <t2@qed.ai>
2020-01-30 14:43:00 +0100

    Unpacking and resizing data

---
+++ prepare_data.sh
@@ -0,0 +1,11 @@
+TRAIN_DIR=data/train/
+TRAIN_IMS=data/train/img.zip
+TRAIN_MASKS=data/train/msk.zip
+
+unzip $TRAIN_IMS -d $TRAIN_DIR
+unzip $TRAIN_MASKS -d $TRAIN_DIR
+
+export PYTHONPATH=$(pwd)
+python src/utils/resize_imagery.py $TRAIN_DIR/msk
+python src/utils/resize_imagery.py $TRAIN_DIR/img
+
---
+++ src/utils/resize_imagery.py
@@ -0,0 +1,27 @@
+import sys
+import numpy as np
+from skimage.io import imread, imsave
+from skimage.transform import resize
+from pathlib import Path
+from tqdm import tqdm
+
+FROM = 543
+TO = 572
+
+
+def main(ims_path):
+    for im_path in tqdm(Path(ims_path).glob("*.tif")):
+        im = imread(im_path)
+        if im.shape[0] == FROM and im.shape[1] == FROM:
+            target_shape = list(im.shape)
+            target_shape[0] = TO
+            target_shape[1] = TO
+            new_im = resize(im, target_shape, order=0, preserve_range=True).astype(
+                im.dtype
+            )
+            assert np.array_equal(np.unique(im), np.unique(new_im))
+            imsave(im_path, new_im, check_contrast=False)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
